fa0d7e22eac6e2843e5f4239fd94814fc9d04ce0
Aidan360 <Aidan36000@gmail.com>
2023-09-24 21:05:46 -0700

    Update startScreen.cpp

--- src/startScreen.cpp
+++ src/startScreen.cpp
@@ -3,5 +3,5 @@
 void vectorR::startScreen::render() {
     createOffset(borderOffset);
     pros::screen::set_pen(COLOR_WHITE);
-    pros::screen::draw_rect(borderOffset + screenX/) // left button
+    pros::screen::draw_rect(borderOffset + screenX/5, borderOffset + screenY/7, borderOffset + 2*(screenX/5), borderOffset + 2*(screenY/7)); // left button
 }
