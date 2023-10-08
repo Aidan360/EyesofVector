406e0ab3c4b1ac71d6506dd7058381c5b24efe7b
Aidan360 <Aidan36000@gmail.com>
2023-10-07 18:18:13 -0700

    fixed objects somehow

--- src/autonmous.cpp
+++ src/autonmous.cpp
@@ -3,28 +3,25 @@
 #include "bac.hpp" 
 #include "pros/adi.hpp"
 #include "pros/misc.h"
-using nameSpace vectorR {
 
-
-    /*  driveTrain.motorPortLeft.push_back(leftFrontMotor_PORT);
-      driveTrain.motorPortLeft.push_back(leftMiddleMotor_PORT);
-      driveTrain.motorPortLeft.push_back(leftBackMotor_PORT);
-      driveTrain.motorPortRight.push_back(rightFrontMotor_PORT);
-      driveTrain.motorPortRight.push_back(rightMiddleMotor_PORT);
-      driveTrain.motorPortRight.push_back(rightBackMotor_PORT);
-    */driveTrain.rpm == 360; 
-      driveTrain.wheelSize = 3.25;
-      driveTrain.trackLength = 13.75; // change when you actually get the robot  */
+    BasicChassis driveTrain; 
 
 void autonomous() {
- //  driveTrain.goForward(40,360);
+  driveTrain.motorPortLeft.push_back(leftFrontMotor_PORT);
+  driveTrain.motorPortLeft.push_back(leftMiddleMotor_PORT);
+  driveTrain.motorPortLeft.push_back(leftBackMotor_PORT);
+  driveTrain.motorPortRight.push_back(rightFrontMotor_PORT);
+  driveTrain.motorPortRight.push_back(rightMiddleMotor_PORT);
+  driveTrain.motorPortRight.push_back(rightBackMotor_PORT);
+  driveTrain.wheelSize = 3.25;
+  driveTrain.trackLength = 13.75; // change when you actually get the robot  */
+  driveTrain.goForward(48,360);
 	 pros::Motor catapultMotor (catapultMotor_PORT);
 	 pros::Motor intakeMotor(intakeMotor_PORT); 
-   intakeMotor.move(-127);
-	pros::delay(2500);
-   intakeMotor.move(127);
-	pros::delay(2500);
-  // driveTrain.goForward(20,360);
-  // driveTrain.turn(1000,360); 
-}
+   //intakeMotor.move(-127);
+	//pros::delay(2500);
+   //intakeMotor.move(127);
+	//pros::delay(2500);
+ // driveTrain.goForward(20,360);
+ // driveTrain.turn(1000,360); 
 }
