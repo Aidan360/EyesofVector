1e05f1fe5d737ef255fb24d535fed2dc38301c6b
Maltuna <41492558+Aidan360@users.noreply.github.com>
2023-10-11 10:22:51 -0700

    Added Horizontal Movement profile to code

--- src/autonmous.cpp
+++ src/autonmous.cpp
@@ -4,7 +4,12 @@
 #include "pros/adi.hpp"
 #include "pros/misc.h"
 
-    BasicChassis driveTrain; 
+BasicChassis driveTrain; 
+
+
+
+
+
 
 void autonomous() {
   driveTrain.motorPortLeft.push_back(leftFrontMotor_PORT);
@@ -16,28 +21,19 @@ void autonomous() {
   driveTrain.wheelSize = 3.25;
   driveTrain.trackLength = 12.75; // change when you actually get the robot  */
 
+  /* 
 
-  /* Track length testing
-   12.75
-   
   */
    //driveTrain.turn(360,360);
-
-
 	 pros::Motor catapultMotor (catapultMotor_PORT);
 	 pros::Motor intakeMotor(intakeMotor_PORT); 
-   //intakeMotor.move_velocity(600);
 
-   //driveTrain.goForward(24,360);
-   driveTrain.turn(180,360);
-   //driveTrain.goForward(24,360);
+
+   driveTrain.goForward(24,360);
+   drivetrain.turn(360,360);
+
+
    pros::delay(1000);
-   //driveTrain.turn(180,360);
 
-   //intakeMotor.move(-127);
-	//pros::delay(2500);
-   //intakeMotor.move(127);
-	//pros::delay(2500);
- // driveTrain.goForward(20,360);
- // driveTrain.turn(1000,360); 
+ 
 }
--- src/bac.cpp
+++ src/bac.cpp
@@ -2,23 +2,70 @@
 #include "main.h"
 #include "pros/motors.hpp"
 #include "math.h"
-void BasicChassis::goForward(double distance, double velocity) {
-    double rotationDegrees = ((distance/wheelSize * 360)/2*-1);
-    double lastDegrees = ((distance/wheelSize * 360)/2*-1)* 0.05;
+double abso(double x) {
+    if (0 > x) {
+        return(x * -1);
+    }
+    else {
+        return(x);
+    }
+}
+ 
+void BasicChassis::goForward(double distance, double velocity) { // Horizontal 
+    double rotationDegrees = ((distance/wheelSize * 360)/2) * -0.90;
+    double lastDegrees = ((distance/wheelSize * 360)/2)* 0.05;
+    
+    for (int i = 0; i < std::size(motorPortLeft); ++i) { 
+        pros::c::motor_tare_position(motorPortLeft[i]);
+        pros::c::motor_tare_position(motorPortRight[i]);
+    }    
     for (int i = 0; i < std::size(motorPortLeft); ++i) {
+        pros::c::motor_move_velocity(motorPortLeft[i],velocity*velVol*-0.3);
+        pros::c::motor_move_velocity(motorPortRight[i],velocity*velVol*-0.3);
+    }
+    while (!((abso(pros::c::motor_get_position(motorPortLeft[0])) > (abso(lastDegrees) - 5)))) {
+    pros::delay(1);
+    } 
+    
+    for (int i = 0; i < std::size(motorPortLeft); ++i) { 
         pros::c::motor_tare_position(motorPortLeft[i]);
         pros::c::motor_tare_position(motorPortRight[i]);
     }
     for (int i = 0; i < std::size(motorPortLeft); ++i) {
-        pros::c::motor_move_relative(motorPortLeft[i],rotationDegrees,velocity*velVol);
-        pros::c::motor_move_relative(motorPortRight[i],rotationDegrees,velocity*velVol);
-    }
-    while (!((pros::c::motor_get_position(motorPortLeft[0])<  rotationDegrees + 5) && (pros::c::motor_get_position(motorPortLeft[0]) > rotationDegrees - 5))) {
+        pros::c::motor_move_velocity(motorPortLeft[i],velocity*velVol*-1);
+        pros::c::motor_move_velocity(motorPortRight[i],velocity*velVol*-1);
+    } 
+
+  
+    while (!((abso(pros::c::motor_get_position(motorPortLeft[0])) > (abso(rotationDegrees) - 5)))) {
     pros::delay(1);
+    } 
+    for (int i = 0; i < std::size(motorPortLeft); ++i) { 
+        pros::c::motor_tare_position(motorPortLeft[i]);
+        pros::c::motor_tare_position(motorPortRight[i]);
     }
+    for (int i = 0; i < std::size(motorPortLeft); ++i) {
+        pros::c::motor_move_velocity(motorPortLeft[i],velocity*velVol*-0.5);
+        pros::c::motor_move_velocity(motorPortRight[i],velocity*velVol*-0.5);
+    }
+    while (!((abso(pros::c::motor_get_position(motorPortLeft[0])) > (abso(lastDegrees) - 5)))) {
+    pros::delay(1);
+    } 
+    for (int i = 0; i < std::size(motorPortLeft); ++i) {
+        pros::c::motor_move_velocity(motorPortLeft[i],0);
+        pros::c::motor_move_velocity(motorPortRight[i],0);
+        pros::c::motor_set_brake_mode(motorPortLeft[i],MOTOR_BRAKE_BRAKE);
+        pros::c::motor_set_brake_mode(motorPortRight[i],MOTOR_BRAKE_BRAKE);
+        pros::c::motor_brake(motorPortLeft[i]);
+        pros::c::motor_brake(motorPortRight[i]);
+    }
+ 
 
 }
-
+ /* for (int i = 0; i < std::size(motorPortLeft); ++i) {
+        pros::c::motor_move_relative(motorPortLeft[i],rotationDegrees,velocity*velVol);
+        pros::c::motor_move_relative(motorPortRight[i],rotationDegrees,velocity*velVol);
+    } */
 void BasicChassis::turn(double degrees, double velocity) {
     double distance = (degrees * M_PI/180) * (trackLength/2);
     double rotationDegrees = (distance/wheelSize * 360)/2 * 0.95;  
@@ -27,13 +74,51 @@ void BasicChassis::turn(double degrees, double velocity) {
         pros::c::motor_tare_position(motorPortLeft[i]);
         pros::c::motor_tare_position(motorPortRight[i]);
     }
+
+for (int i = 0; i < std::size(motorPortLeft); ++i) {
+        pros::c::motor_move_velocity(motorPortLeft[i],velocity*velVol*-0.3);
+        pros::c::motor_move_velocity(motorPortRight[i],velocity*velVol*0.3);
+    }
+    while (!((abso(pros::c::motor_get_position(motorPortLeft[0])) > (abso(lastDegrees) - 5)))) {
+    pros::delay(1);
+    } 
+    
+    for (int i = 0; i < std::size(motorPortLeft); ++i) { 
+        pros::c::motor_tare_position(motorPortLeft[i]);
+        pros::c::motor_tare_position(motorPortRight[i]);
+    }
     for (int i = 0; i < std::size(motorPortLeft); ++i) {
+        pros::c::motor_move_velocity(motorPortLeft[i],velocity*velVol*-1);
+        pros::c::motor_move_velocity(motorPortRight[i],velocity*velVol*1);
+    } 
+
+  
+    while (!((abso(pros::c::motor_get_position(motorPortLeft[0])) > (abso(rotationDegrees) - 5)))) {
+    pros::delay(1);
+    } 
+    for (int i = 0; i < std::size(motorPortLeft); ++i) { 
+        pros::c::motor_tare_position(motorPortLeft[i]);
+        pros::c::motor_tare_position(motorPortRight[i]);
+    }
+    for (int i = 0; i < std::size(motorPortLeft); ++i) {
+        pros::c::motor_move_velocity(motorPortLeft[i],velocity*velVol*-0.5);
+        pros::c::motor_move_velocity(motorPortRight[i],velocity*velVol*0.5);
+    }
+    while (!((abso(pros::c::motor_get_position(motorPortLeft[0])) > (abso(lastDegrees) - 5)))) {
+    pros::delay(1);
+    } 
+    for (int i = 0; i < std::size(motorPortLeft); ++i) {
+        pros::c::motor_move_velocity(motorPortLeft[i],0);
+        pros::c::motor_move_velocity(motorPortRight[i],0);
+        pros::c::motor_set_brake_mode(motorPortLeft[i],MOTOR_BRAKE_BRAKE);
+        pros::c::motor_set_brake_mode(motorPortRight[i],MOTOR_BRAKE_BRAKE);
+        pros::c::motor_brake(motorPortLeft[i]);
+        pros::c::motor_brake(motorPortRight[i]);
+    }
+    /* for (int i = 0; i < std::size(motorPortLeft); ++i) {
         pros::c::motor_move_absolute(motorPortLeft[i],-rotationDegrees,(-1*velocity*velVol));
         pros::c::motor_move_absolute(motorPortRight[i],rotationDegrees,(velocity*velVol));
-    }  
-    while (!((pros::c::motor_get_position(motorPortLeft[0])<  rotationDegrees + 5) && (pros::c::motor_get_position(motorPortLeft[0]) > rotationDegrees - 5))) {
-    pros::delay(1);
-    }
+    }  */
 
 }
 
